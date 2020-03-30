d07ab279bc9ad07238556a4d3ed0f7206b91f008
Steven Smith <steven.76484@gmail.com>
2020-03-30 17:59:38 -0400

    Implementing global store with courses data using Redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
 
 class CoursesPage extends React.Component {
   state = {
     course: {
       title: ""
     }
+  };
+
+  static propTypes = {
+    courses: PropTypes.arrayOf(PropTypes.object).isRequired,
+    dispatch: PropTypes.func.isRequired
   };
 
   handleChange = event => {
@@ -14,7 +22,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    console.log(this.state.course.title);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   };
 
   render() {
@@ -28,9 +36,18 @@ class CoursesPage extends React.Component {
           value={this.state.course.title}
         />
         <input type="submit" value="Save" />
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
       </form>
     );
   }
 }
 
-export default CoursesPage;
+const mapStateToProps = state => {
+  return {
+    courses: state.courses
+  };
+};
+
+export default connect(mapStateToProps)(CoursesPage);
