f28149b994469ac831f6ef367e195cd237e256d8
Steven Smith <steven.76484@gmail.com>
2020-03-30 13:46:07 -0400

    Adding Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+import Header from "./common/Header";
 import AboutPage from "./about/AboutPage";
 import HomePage from "./home/HomePage";
 
 const App = () => (
   <div className="container-fluid">
+    <Header />
     <Route exact path="/" component={HomePage} />
     <Route path="/about" component={AboutPage} />
   </div>
---
+++ src/components/common/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const activeStyle = {
+  color: "#f15b2a"
+};
+
+const Header = () => (
+  <nav>
+    <NavLink to="/" activeStyle={activeStyle} exact>
+      Home
+    </NavLink>
+    {" | "}
+    <NavLink to="/about" activeStyle={activeStyle}>
+      About
+    </NavLink>
+  </nav>
+);
+
+export default Header;
