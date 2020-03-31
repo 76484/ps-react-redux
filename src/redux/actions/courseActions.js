807c1e93f938ddfe79741c48d6ed886d89675040
Steven Smith <steven.76484@gmail.com>
2020-03-30 21:05:00 -0400

    Adding  getCourses action creator

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,8 +1,25 @@
 import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return {
     type: types.CREATE_COURSE,
     course
+  };
+}
+
+export function getCourses() {
+  return function(dispatch) {
+    return courseApi
+      .getCourses()
+      .then(courses => {
+        dispatch({
+          type: types.GET_COURSES_SUCCESS,
+          courses
+        });
+      })
+      .catch(error => {
+        throw error; // TODO: Handle error
+      });
   };
 }
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -4,6 +4,8 @@ export default function courseReducer(state = [], action) {
   switch (action.type) {
     case types.CREATE_COURSE:
       return [...state, { ...action.course }];
+    case types.GET_COURSES_SUCCESS:
+      return action.courses;
     default:
       return state;
   }
