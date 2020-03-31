535c64d867c3bb7312d81600ae5f192c90dd21d5
Steven Smith <steven.76484@gmail.com>
2020-03-30 21:05:25 -0400

    Updating Courses Page to get courses on mount

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,42 +6,25 @@ import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ""
-    }
-  };
-
   static propTypes = {
     actions: PropTypes.object.isRequired,
     courses: PropTypes.arrayOf(PropTypes.object).isRequired
   };
 
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
+  componentDidMount() {
+    this.props.actions.getCourses().catch(error => {
+      alert(`Loading courses failed: ${error}`);
+    });
+  }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          onChange={this.handleChange}
-          type="text"
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map(course => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
