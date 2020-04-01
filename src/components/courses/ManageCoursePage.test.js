586dbfd2194227e3ea56627400845c1f1c79c2f6
Steven Smith <steven.76484@gmail.com>
2020-04-01 12:56:45 -0400

    Adding test of title field validation in ManageCoursePage

---
+++ src/components/courses/ManageCoursePage.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { mount } from "enzyme";
+import { authors, newCourse, courses } from "../../../tools/mockData";
+import { ManageCoursePage } from "./ManageCoursePage";
+
+function render(args) {
+  const defaultProps = {
+    authors,
+    course: newCourse,
+    courses,
+    history: {},
+    match: {},
+    getAuthors: jest.fn(),
+    getCourses: jest.fn(),
+    saveCourse: jest.fn()
+  };
+
+  const props = { ...defaultProps, ...args };
+  return mount(<ManageCoursePage {...props} />);
+}
+
+it("sets error when attempting to save an empty title field", () => {
+  const wrapper = render();
+
+  wrapper.find("form").simulate("submit");
+
+  const error = wrapper.find(".alert").first();
+
+  expect(error.text()).toBe("Title is required");
+});
