5dd991e2e7d47faecfaded2356eac305c2f92dab
Steven Smith <steven.76484@gmail.com>
2020-04-01 15:53:58 -0400

    Adding prod build

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,3 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
-import thunk from "redux-thunk";
-
-export default function configureStore(initialState) {
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-  );
-}
+module.exports = require(process.env.NODE_ENV === "production"
+  ? "./configureStore.prod"
+  : "./configureStore.dev");
